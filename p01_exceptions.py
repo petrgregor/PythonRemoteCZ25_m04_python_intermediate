96e86ac1506b1140d7cb9c11b89bec7499cc8317
Petr Gregor <petr.gregor@email.cz>
2024-10-08 19:59:15 +0200

    Exceptions - part 2.

--- p01_exceptions.py
+++ p01_exceptions.py
@@ -1,3 +1,5 @@
+from math import log
+
 a = 3
 b = [1, 0, 2]
 for elem in b:
@@ -32,6 +34,58 @@ while i < 4:
         #i += 1
     finally:
         i += 1
+
+print("-" * 80)
+
+
+def divide1(a, b):
+    if b != 0:
+        return a/b
+    return None
+
+
+def divide2(a, b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return None
+
+
+def divide3(a, b):
+    return a/b
+
+
+def divide_all(a, b_list):
+    for elem in b_list:
+        try:
+            result = divide3(a, elem)
+            print(f"{a}/{elem} = {result}")
+        except ZeroDivisionError:
+            print("Error: dělení nulou")
+        except TypeError:
+            print("Error: TypeError")
+
+
+def divide_all2(a, b_list):
+    for elem in b_list:
+        result = divide3(a, elem)
+        print(f"{a}/{elem} = {result}")
+
+
+b_list = [1, 2, 3, 0, 5, 6, 7, "8", "Ahoj"]
+divide_all(12, b_list)
+print("-" * 40)
+try:
+    divide_all2(25, b_list)
+except ZeroDivisionError:
+    print("Error: nelze dělit nulou.")
+
+num1 = -16
+print(f"sqrt({num1}) = {num1 ** (1/2)}")
+try:
+    print(f"log({num1}) = {log(num1)}")
+except ValueError:
+    print("Error: nelze počítat logaritmus pro záporné číslo.")
 
 print("=" * 80)
 print("Konec")
